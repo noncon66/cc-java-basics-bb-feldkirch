c246d499eed4b078e188e1f2712867621726f292
noncon66 <klaus.nu@cable.vol.at>
2020-10-20 21:53:52 +0200

    initial

--- Oktober/src/ZufallsSumme.java
+++ Oktober/src/ZufallsSumme.java
@@ -7,15 +7,13 @@ public class ZufallsSumme {
     public static void main(String[] args) {
         int sum = 0;
         int adder;
-        while (true){
+        while (true) {
             Random random = new Random();
             adder = random.nextInt(21) + 10;
             if (adder == 15 || adder == 25) break;
             sum = sum + adder;
             System.out.println("+" + adder + " = " + sum);
         }
-
-
         System.out.println(sum);
     }
 
