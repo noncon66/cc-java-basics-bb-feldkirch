1031a6b60ed77ec70c3e1d27de998f1cbda683c7
noncon66 <klaus.nu@cable.vol.at>
2020-10-27 18:19:39 +0100

    initial commit

---
+++ Oktober/src/Wichteln.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+public class Wichteln {
+
+    public static void main(String[] args) {
+        String[] names = {"Ali", "Erich", "Lukas", "Dan", "Klaus", "Bokhee", "Irene", "Sabrina"};
+        String[] assigned = new String[names.length];
+        Random ramdom = new Random();
+
+        for (int i = 0; i < names.length; i++) {
+
+            boolean looping = true;
+            while (looping) {
+                String randomname = names[ramdom.nextInt(names.length)];
+                if (!isAlreadyUsed(assigned, randomname) && !names[i].equals(randomname)) {
+                    assigned[i] = randomname;
+                    System.out.println(names[i] + " --> " + randomname);
+                    looping = false;
+                }
+            }
+
+        }
+    }
+
+    private static boolean isAlreadyUsed(String[] assigned, String randomname) {
+        boolean returner = false;
+        for (var name : assigned) {
+            if (randomname.equals(name)) {
+                returner = true;
+                break;
+            } else {
+                returner = false;
+            }
+
+        }
+        return returner;
+
+    }
+
+
+}
+
+
+
+
+
+
