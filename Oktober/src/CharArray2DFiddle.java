bc894e18e6ebe5458aeeeb908f0f64fbc5dbe60c
noncon66 <klaus.nu@cable.vol.at>
2020-11-07 13:09:03 +0100

    added drawLine();drawRectangleFromLines(); drawCircle();

--- Oktober/src/CharArray2DFiddle.java
+++ Oktober/src/CharArray2DFiddle.java
@@ -10,7 +10,14 @@ public class CharArray2DFiddle {
         //print2DArray(field);
         //insertSquare(field, 10);
         //insertMiddleSquare(field, 20);
-        drawLine(field,5,5, 10,20);
+        //drawLine(field, 0, 0, 20, 0);
+        //drawLine(field, 0, 0, 0, 20);
+        //drawLine(field, 20, 0, 20, 20);
+        //drawLine(field, 0, 20, 20, 20);
+        //drawRectangleFromLines(field, 10, 40);
+        drawCircle(field, 10);
+        drawCircle(field, 5);
+        drawCircle(field, 15);
         print2DArray(field);
     }
 
@@ -72,17 +79,64 @@ public class CharArray2DFiddle {
         return field;
     }
 
-    private static char[][] drawLine(char[][] field, int startX, int startY, int endX, int endY) {
-        int deltaX = endX - startX;
-        int deltaY = endY - startY;
+    private static char[][] drawLine(char[][] field, int startRow, int startCol, int endRow, int endCol) {
+        int deltaRow = endRow - startRow;
+        int deltaCol = endCol - startCol;
+        float pitch = (deltaCol != 0) ? (float) deltaRow / (float) deltaCol : deltaRow * 1000;
+        int directionRow = (deltaRow != 0) ? deltaRow / Math.abs(deltaRow) : 0;
+        int directionCol = (deltaCol != 0) ? deltaCol / Math.abs(deltaCol) : 0;
 
-        int x = startX;
-        int y = startY;
-        while (x <= endX && y <= endY ){
-            field[x][y] = 'o';
-            x = x + deltaY/deltaX;
-            y = y + deltaX/deltaY;
+        if (Math.abs(pitch) <= 1) {
+            for (int col = startCol; col != endCol; col += directionCol) {
+                int row = startRow + Math.round((col - startCol) * pitch);
+                field[row][col] = 'x';
+            }
+
+        } else {
+            for (int row = startRow; row != endRow; row += directionRow) {
+                int col = startCol + Math.round((row - startRow) / pitch);
+                field[row][col] = 'x';
+            }
         }
+        field[endRow][endCol] = 'x';
+        return field;
+    }
+
+    private static char[][] drawRectangleFromLines(char[][] field, int width, int height) {
+        drawLine(field, 0, 0, 0, width - 1);
+        drawLine(field, 0, width - 1, height - 1, width - 1);
+        drawLine(field, height - 1, width - 1, height - 1, 0);
+        drawLine(field, height - 1, 0, 0, 0);
+        return field;
+
+    }
+
+    private static char[][] drawCircle(char[][] field, int radius) {
+        int center = field.length / 2;
+        int row = 0;
+        int col = 0;
+        int currentRow = 0;
+        int currentCol = 0;
+        float deltaCols = 0;
+        float deltaRows = 0;
+
+        //Zeichne Kreis über die Spalten (cols)
+        for (col = center - radius; col <= center + radius; col++) {
+            row = (int) (center + Math.round(Math.sqrt(Math.pow(radius, 2) - Math.pow(radius - currentRow, 2))));
+            field[row][col] = 'x';
+            row = (int) (center - Math.round(Math.sqrt(Math.pow(radius, 2) - Math.pow(radius - currentRow, 2))));
+            field[row][col] = 'x';
+            currentRow++;
+        }
+        //Zeichne Kreis nochmal über die Zeilen (rows)
+        for (row = center - radius; row <= center + radius; row++) {
+            col = (int) (center + Math.round(Math.sqrt(Math.pow(radius, 2) - Math.pow(radius - currentCol, 2))));
+            field[row][col] = 'x';
+            col = (int) (center - Math.round(Math.sqrt(Math.pow(radius, 2) - Math.pow(radius - currentCol, 2))));
+            field[row][col] = 'x';
+            currentCol++;
+        }
+
         return field;
     }
 
